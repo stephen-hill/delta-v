08664268108377a8d6175e3e3e0526b420fa7b31
Stephen Hill <stephen@gatekiller.co.uk>
2013-07-17 16:50:44 +0100

    Added autoloader for classes.

---
+++ autoloader.php
@@ -0,0 +1,22 @@
+<?php
+
+spl_autoload_register(function($class)
+{
+	$directories = array(
+		dirname(__FILE__) . DIRECTORY_SEPARATOR . "models"
+	);
+	
+	$path = $class . ".php";
+	$path = str_replace("\\", DIRECTORY_SEPARATOR, $path);
+	$path = str_replace("_", DIRECTORY_SEPARATOR, $path);
+	
+	foreach ($directories as $dir)
+	{
+		$fullpath = $dir . DIRECTORY_SEPARATOR . $path;
+				
+		if (stream_resolve_include_path($fullpath) !== false)
+		{
+			require_once($fullpath);
+		}
+	}
+});
