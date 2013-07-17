a0b115a35d872fac1c90c2cc6822690b4e65efc3
Stephen Hill <stephen@gatekiller.co.uk>
2013-07-17 16:50:03 +0100

    Finish draft of importing files.

--- daemon.php
+++ daemon.php
@@ -2,25 +2,37 @@
 
 namespace StephenHill\DeltaV
 {
+	require_once('autoloader.php');
+	$database = new Database();
+	
+	// Set 1: Hash and move the uploaded files. Insert them into the database.
 	$pattern = realpath(__DIR__) . '/upload/*.[mM][pP]3';
 	$music = realpath(__DIR__) . '/music';
-
 	$files = glob($pattern, GLOB_NOSORT);
-
 
 	foreach ($files as $file)
 	{
 		$hash = hash_file('sha256', $file);
 		$new = $music . '/' . $hash;
+		
 		if (file_exists($new))
 		{
-			echo "exists\n";
-		}
-		else
-		{
-			copy($file, $new);
-			echo 'created';
+			echo "skipping\n";
+			unlink($file);
+			continue;
 		}
 		
+		rename($file, $new);
+		echo "created\n";
+		
+		echo $hash . PHP_EOL;
+		
+		$object = array
+		(
+			'id' => $hash,
+			'filename' => basename($file)
+		);
+		
+		$database->Add('files', $object);
 	}
 }
