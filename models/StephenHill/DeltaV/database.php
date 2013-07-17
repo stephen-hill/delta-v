0202c39e5adab2d86aedc9de7c8d0fa15e0591f2
Stephen Hill <stephen@gatekiller.co.uk>
2013-07-17 15:22:07 +0100

    Moved.

---
+++ models/StephenHill/DeltaV/database.php
@@ -0,0 +1,45 @@
+<?
+
+namespace StephenHill\DeltaV
+{
+	class Database
+	{
+		public $Connection;
+		
+		public function __construct()
+		{
+			$this->Connection = new PDO('mysql:dbname=deltav;host=localhost', 'deltav', 'deltav');
+		}
+		
+		public function GetByID($table, $id)
+		{
+			
+		}
+		
+		public function Add($table, $kvp)
+		{
+			$query = 'INSERT INTO ' . $table . ' SET ';
+			
+			foreach($kvp as $key => $value)
+			{
+				$query .= "$key = :$key";
+				
+				end($kvp);
+				
+				if ($key !== key($kvp))
+				{
+					$query .= ','
+				}
+			}
+			
+			$statement = $this->Connection->prepare($query);
+			
+			foreach($kvp as $key => $value)
+			{
+				$statement->bindParam(':' . $key, $value);
+			}
+			
+			return $statement->execute();
+		}
+	}
+}
