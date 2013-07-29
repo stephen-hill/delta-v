d922a86c93df00c169b20e9903a2107ab7a7e2e3
Stephen Hill <stephen@gatekiller.co.uk>
2013-07-29 12:33:42 +0100

    Now extracts ID3 information and adds it to the database. Still need to do alot of error checking.

--- daemon.php
+++ daemon.php
@@ -2,8 +2,12 @@
 
 namespace StephenHill\DeltaV
 {
+	use \getID3;
+	
 	require_once('autoloader.php');
 	$database = new Database();
+	$getID3 = new getID3();
+	$getID3->setOption(array('encoding' => 'UTF-8'));
 	
 	// Set 1: Hash and move the uploaded files. Insert them into the database.
 	$pattern = realpath(__DIR__) . '/upload/*.[mM][pP]3';
@@ -12,27 +16,62 @@ namespace StephenHill\DeltaV
 
 	foreach ($files as $file)
 	{
-		$hash = hash_file('sha256', $file);
+		$hash = hash_file('md5', $file);
 		$new = $music . '/' . $hash;
 		
 		if (file_exists($new))
 		{
-			echo "skipping\n";
 			unlink($file);
 			continue;
 		}
 		
 		rename($file, $new);
-		echo "created\n";
 		
-		echo $hash . PHP_EOL;
+		$id3 = $getID3->analyze($file);
 		
 		$object = array
 		(
 			'id' => $hash,
-			'filename' => basename($file)
+			'filename' => basename($file),
+			'bit-rate' => (int)$id3['audio']['bitrate'],
+			'sample-rate' => (int)$id3['audio']['sample_rate'],
+			'channels' => (int)$id3['audio']['channels'],
+			'size' => filesize($file),
+			'duration' => (int)round($id3['playtime_seconds']),
+			'title' => $id3['tags']['id3v2']['title'][0],
+			'artist' => $id3['tags']['id3v2']['artist'][0],
+			'added' => date("Y-m-d H:i:s"),
+			'content-type' => $id3['mime_type'],
+			'favorite' => 0,
+			'playtime' => 0,
+			'plays' => 0,
+			'lastplay' => date("Y-m-d H:i:s")
 		);
 		
+		if (array_key_exists('year', $id3['tags']['id3v2']) === true)
+		{
+			$object['year'] = (int)$id3['tags']['id3v2']['year'][0];
+		}
+		
+		if (array_key_exists('track_number', $id3['tags']['id3v2']) === true)
+		{
+			$object['track'] = (int)$id3['tags']['id3v2']['track_number'][0];
+		}
+		
+		if (array_key_exists('album', $id3['tags']['id3v2']) === true)
+		{
+			$object['album'] = $id3['tags']['id3v2']['album'][0];
+		}
+		
+		if (array_key_exists('band', $id3['tags']['id3v2']) === true)
+		{
+			$object['album-arist'] = $id3['tags']['id3v2']['band'][0];
+		}
+		
 		$database->Add('files', $object);
+				
+		//var_dump($id3);
+		
+		var_dump($object);
 	}
 }
