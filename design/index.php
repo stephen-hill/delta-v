0c44b58ada747df038c0badfd2d0f1577199fe50
Stephen Hill <stephen@gatekiller.co.uk>
2013-07-29 16:52:41 +0100

    Design html file.

---
+++ design/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>DeltaV</title>
+		<meta charset="utf-8" />
+	</head>
+	<body>
+		<div class="layout-logo">
+			<a href="#"><img src="logo.png" /></a>
+		</div>
+		
+		<div class="layout-playing">
+			<h1>Sloan's Assault by Power Glove - Far Cry 3 Blood Dragon OST</h1>
+			<img src="progressbar.png" />
+		</div>
+		
+		<div class="layout-controls">
+			<ul class="controls-playpause">
+				<li><a href="#"><img src="previous.png" /></a></li>
+				<li><a href="#"><img src="play.png" /></a></li>
+				<li><a href="#"><img src="next.png" /></a></li>
+			</ul>
+			
+			<ul class="controls-shufflerandom">
+				<li><a href="#"><img src="shuffle.png" /></a></li>
+				<li><a href="#"><img src="random.png" /></a></li>
+			</ul>
+		</div>
+		
+		<div class="layout-search">
+			<form>
+				<input type="search" />
+			</form>
+		</div>
+		
+		<div class="layout-library">
+			<h2>Library</h2>
+			<ul>
+				<li><a href="#">All</a></li>
+				<li><a href="#">Albums</a></li>
+				<li><a href="#">Artists</a></li>
+				<li><a href="#">Years</a></li>
+				<li><a href="#">Genres</a></li>
+			</ul>
+		</div>
+		
+		<div class="layout-playlists">
+			<h2>Playlists</h2>
+			<ul>
+				<li><a href="#">Favorites</a></li>
+				<li><a href="#">Recently Added</a></li>
+				<li><a href="#">Most Played</a></li>
+				<li><a href="#">My Playlist</a></li>
+			</ul>
+		</div>
+		
+		<div class="layout-tracks">
+			<ul>
+				<li>Track</li>
+				<li>Title</li>
+				<li>Arist</li>
+				<li>Album</li>
+				<li>Genre</li>
+				<li>Duration</li>
+			<ul>
+			<ol>
+				<li>1</li>
+				<li>Rex Colt</li>
+				<li>Power Glove</li>
+				<li>Far Cry 3 Blood Dragon OST</li>
+				<li>Soundtrack</li>
+				<li>01:47</li>
+			</ol>
+		</div>
+			
+	</body>
+</html>
